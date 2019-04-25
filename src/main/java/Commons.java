99b1682324bacd7f788755417e01965ce5bbc224
Kinollo <kinski.marcin@gmail.com>
2019-04-25 19:46:07 +0200

    Arkanoid game. Improve some details

---
+++ src/main/java/Commons.java
@@ -0,0 +1,13 @@
+public interface Commons {
+
+    public static final int WIDTH = 600;
+    public static final int HEIGTH = 500;
+    public static final int BOTTOM_EDGE = 490;
+    public static final int N_OF_BRICKS = 60;
+    public static final int INIT_PADDLE_X = 300;
+    public static final int INIT_PADDLE_Y = 360;
+    public static final int INIT_BALL_X = 330;
+    public static final int INIT_BALL_Y = 355;
+    public static final int DELAY = 1000;
+    public static final int PERIOD = 10;
+}
